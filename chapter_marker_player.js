7b2cc78df2a200abcad9b3955fe8ac84e7b71e93
Evelyn Rios <everios@me.com>
2014-05-28 13:33:56 -0700

    Update chapter_marker_player.js

--- chapter_marker_player.js
+++ chapter_marker_player.js
@@ -1,5 +1,7 @@
 /*
  Copyright 2012 Google Inc. All Rights Reserved.
+ Modifications by Evelyn Rios
+ Copyright 2014 Evelyn Rios
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
